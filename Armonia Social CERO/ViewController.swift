41778dc2532212a4d750f793e267764837ee9018
mbahfonong <PonchoTG@paranoid.local>
2017-06-04 20:46:39 -0400

    Initial Commit

---
+++ Armonia Social CERO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Armonia Social CERO
+//
+//  Created by Alfonso Tapia on 6/4/17.
+//  Copyright © 2017 Paranoid Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
